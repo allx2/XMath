341d50049fcbf1a008d47518a1bfaa4c16f9e82a
allx2 <nallx2@aol.com>
2016-04-28 18:31:01 -0500

    add project stuff

--- main.cpp
+++ main.cpp
@@ -10,16 +10,16 @@ int main(int argc, const char *argv[])
 {
     int rows,columns;
     cout<<"introducir el grado del polinomio"<<endl;
-	polinomio A(0),B(0);
-	cin>>A;
-	cout<<A;
-	cout<<endl;
-	B=derivada(A);
-	cout<<B;
+    polinomio A(0),B(0);
+    cin>>A;
+    cout<<A;
+    cout<<endl;
+    B=derivada(A);
+    cout<<B;
     system("pause");
-	cout<<"evaluacion de los polinomios";
-	cout<<A.evaluar(1)<<endl;
-	cout<<B.evaluar(1);
-	system("pause");
+    cout<<"evaluacion de los polinomios";
+    cout<<A.evaluar(1)<<endl;
+    cout<<B.evaluar(1);
+    system("pause");
     return 0;
 }
